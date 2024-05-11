2e94f320ba1486b08315ffe3989e32e278107de4
Rafael Rosario <rafa.rosario2908@gmail.com>
2024-05-10 21:50:32 -0300

    ENTRADA DE DADOS

--- src/Classes/Transacao.java
+++ src/Classes/Transacao.java
@@ -6,7 +6,7 @@ public class Transacao {
     private int idCaixaEletronico;
     private int dataHora;
     private double valor;
-
+    // (C- credito D-debito)
     private char tipoTransacao;
 
 }
--- src/Classes/conta.java
+++ src/Classes/conta.java
@@ -5,9 +5,66 @@ public class conta {
     private int agencia;
     private int digitoVerificador;
     private String nome;
-    private String CPF;
+    private String cpf;
     // (1-conta |corrente 2-conta salario 3-conta poupança)
-    private int tituloConta;
+    private int tipoConta;
     private String numeroCartao;
     private String senha;
+
+
+    public conta(int numero, int agencia, int digitoVerificador, String nome, String cpf, int tipoConta,
+            String numeroCartao, String senha) {
+        this.numero = numero;
+        this.agencia = agencia;
+        this.digitoVerificador = digitoVerificador;
+        this.nome = nome;
+        this.cpf = cpf;
+        this.tipoConta = tipoConta;
+        this.numeroCartao = numeroCartao;
+        this.senha = senha;
+    }
+
+
+    public int getNumero() {
+        return numero;
+    }
+
+
+    public int getAgencia() {
+        return agencia;
+    }
+
+
+    public int getDigitoVerificador() {
+        return digitoVerificador;
+    }
+
+
+    public String getNome() {
+        return nome;
+    }
+
+
+    public String getCpf() {
+        return cpf;
+    }
+
+
+    public int getTipoConta() {
+        return tipoConta;
+    }
+
+
+    public String getNumeroCartao() {
+        return numeroCartao;
+    }
+
+
+    public String getSenha() {
+        return senha;
+    }
+
+
+
+    
 }
