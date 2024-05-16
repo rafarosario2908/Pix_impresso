86aee0be8f33b0c07a07e53bc4de45bec6b4f03e
Rafael Rosario <rafa.rosario2908@gmail.com>
2024-05-16 20:57:48 -0300

    validaçao do caixas

--- src/App.java
+++ src/App.java
@@ -1,60 +1,125 @@
+
 import java.util.ArrayList;
 import java.util.Scanner;
 
-import Classes.conta;
+import Classes.CaixaEletronico;
+import Classes.Conta;
+import Classes.ContaCorrente;
+import Classes.ContaPoupanca;
+import Classes.ContaSalario;
 
 public class App {
     public static void main(String[] args) throws Exception {
+        ArrayList<Conta> contas = new ArrayList<Conta>();
 
-        ArrayList<conta> contas = new ArrayList<conta>();
-        contas.add(new conta(1,
+        contas.add(new ContaCorrente(1,
                 123,
                 1234,
-                "Rafael Rosario",
-                "12345678910",
-                1234567899,
-                "123245",
-                "1234"));
+                0,
+                "Rafael rosario",
+                "123.456.789-00",
+                "123456789",
+                "123"));
+
+        contas.add(new ContaSalario(2,
+                123,
+                1234,
+                0,
+                "Rafael rosario",
+                "123.456.789-00",
+                "123456789",
+                "123"));
+        contas.add(new ContaPoupanca(3,
+                123,
+                1234,
+                0,
+                "Rafael rosario",
+                "123.456.789-00",
+                "123456789",
+                "123"));
+
+        // TODO - Criar uma lista de caixas eletronicos
+        ArrayList<CaixaEletronico> caixaEletronicos = new ArrayList<CaixaEletronico>();
+
+        // TODO - Criar objetos do tipo caixa eletronico
+        caixaEletronicos.add(new CaixaEletronico(1,
+                "Pato Branco",
+                10.000));
+        caixaEletronicos.add(new CaixaEletronico(2,
+                "Clevelandia",
+                10.000));
+        caixaEletronicos.add(new CaixaEletronico(3,
+                "Mariopolis",
+                10.000));
+
         Scanner scanner = new Scanner(System.in);
-        // Entrada
-        // 1 - Ir até um caixa eletronico
-        // 2 - Inserir 0 Cartão
-        // 2.1 validar os dados dos cartâo
-        System.out.println("insira o numero do cartão: ");
+        // ENTRADA
+        // 1 - Ir até um caixa eletrônico
+        // 2 - Inserir o cartão
+        // 2.1 - Ler os dados do cartão
+        // se o número do cartão for igual ao número do cartão de uma conta
+        System.out.println("Insira o numero do cartão: ");
         String numeroCartao = scanner.nextLine();
 
-        conta contaSaque = null;
-        for (conta conta : contas) {
+        Conta contaSaque = null;
+        for (Conta conta : contas) {
             if (conta.getNumeroCartao().equals(numeroCartao)) {
                 contaSaque = conta;
+                break;
+            }
+        }
+
+        if (contaSaque == null) {
+            scanner.close();
+            throw new Error("Conta não encontrada");
+        }
+
+        // 3 - Inserir a senha
+        // 4 - Validar a senha (processamento intermediario)
+        // se a senha estiver incorreta, vai mostrar um erro e terminar o programa
+        System.out.println("Insira a senha: ");
+        String senha = scanner.nextLine();
+
+        if (!contaSaque.getSenha().equals(senha)) {
+            scanner.close();
+            throw new Error("Senha inválida");
+        }
+
+        // TODO - 5.1 - Informar o id do Caixa Eletronico
+
+        System.out.println("Insira o numero do terminal: ");
+        int terminal = scanner.nextInt();
+
+        CaixaEletronico localCaixa = null;
+
+        for (CaixaEletronico caixaEletronico : caixaEletronicos) {
+
+            if (caixaEletronico.getIdentificador() == terminal) {
+                localCaixa = caixaEletronico;
                 break;
 
             }
 
-            if (contaSaque == null) {
-                throw new Error("conta não encontrada");
-            }
+        }
+
+        if (localCaixa == null) {
+            scanner.close();
+            throw new Error("terminal nao encontrado");
 
         }
 
-        // 3 - Inserir a senha
-        System.out.println("insira a sua senha: ");
-        String senha = scanner.nextLine();
-
-        
-
-        // 4 - Validar a Senha
-        if (!contaSaque.getSenha().equals(senha)) {
-            throw new Error("Senha incorreta! ");
-            
-        }
         // 5 - Informar o valor do saque
-        // processamento
+
+        // TODO - 5.2 - Buscar o caixa eletronico na lista
+
+        // PROCESSAMENTO
         // 6 - Validar se o caixa possui saldo
         // 7 - Validar se a conta possui saldo
         // 8 - Liberar o dinheiro para o cliente
-        // 9 - Registrar a Transação
-        // 10 - Encerrar a operação
+        // 9 - Registrar a transação
 
+        // SAÍDA
+        // 10 - Encerrar a operação
+        // 10.1 - Mostrar o comprovante da transação em tela
     }
 }
